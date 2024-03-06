4a13f25f5104c6a2b1c93224ab1190bb3dfa72c2
guiemhomar <ghomar@nigul.cide.es>
2024-03-06 15:07:02 +0100

    asdsd

--- app/src/main/java/legendary/lamp/App.java
+++ app/src/main/java/legendary/lamp/App.java
@@ -8,6 +8,8 @@ public class App {
         return "Hello World!";
     }
 
+    public int sumar(int n1, int n2)
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
